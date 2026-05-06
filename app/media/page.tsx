9898608db5a8b6bc3393024889eecbdef9291dd9
Guillechu <guillechupears05@gmail.com>
2026-05-05 22:27:59 -0500

    Fix media video type error

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -95,7 +95,7 @@ export default function MediaPage() {
         ) : (
           <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {filtered.map((h) => {
-              const isLocalVideo = Boolean(h.video);
+              const isLocalVideo = Boolean((h as any).video);
 
               return (
                 <article
